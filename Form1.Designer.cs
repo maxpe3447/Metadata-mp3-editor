3c039e36b2efd56dfbdac598e961a6735a1a1a86
admin <admin@ADMIN->
2021-05-30 22:00:02 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace Visual_Mp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxTittle = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxArtist = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBoxAlbum = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxName = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBoxDuration = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(478, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This program can read tegs from your music file!";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(17, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choice File!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(14, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tittle:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Artist:";
+            // 
+            // richTextBoxTittle
+            // 
+            this.richTextBoxTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxTittle.Location = new System.Drawing.Point(122, 20);
+            this.richTextBoxTittle.Name = "richTextBoxTittle";
+            this.richTextBoxTittle.Size = new System.Drawing.Size(250, 40);
+            this.richTextBoxTittle.TabIndex = 4;
+            this.richTextBoxTittle.Text = "";
+            this.richTextBoxTittle.TextChanged += new System.EventHandler(this.richTextBoxTittle_TextChanged);
+            // 
+            // richTextBoxArtist
+            // 
+            this.richTextBoxArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxArtist.Location = new System.Drawing.Point(122, 85);
+            this.richTextBoxArtist.Name = "richTextBoxArtist";
+            this.richTextBoxArtist.Size = new System.Drawing.Size(250, 40);
+            this.richTextBoxArtist.TabIndex = 5;
+            this.richTextBoxArtist.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(14, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Album:";
+            // 
+            // richTextBoxAlbum
+            // 
+            this.richTextBoxAlbum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxAlbum.Location = new System.Drawing.Point(122, 141);
+            this.richTextBoxAlbum.Name = "richTextBoxAlbum";
+            this.richTextBoxAlbum.Size = new System.Drawing.Size(250, 40);
+            this.richTextBoxAlbum.TabIndex = 7;
+            this.richTextBoxAlbum.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBoxName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.richTextBoxDuration);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.richTextBoxAlbum);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.richTextBoxArtist);
+            this.groupBox1.Controls.Add(this.richTextBoxTittle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(323, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 339);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Music Info";
+            // 
+            // richTextBoxName
+            // 
+            this.richTextBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxName.Location = new System.Drawing.Point(122, 187);
+            this.richTextBoxName.Name = "richTextBoxName";
+            this.richTextBoxName.Size = new System.Drawing.Size(250, 90);
+            this.richTextBoxName.TabIndex = 11;
+            this.richTextBoxName.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(0, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 26);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Comment:";
+            // 
+            // richTextBoxDuration
+            // 
+            this.richTextBoxDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxDuration.Location = new System.Drawing.Point(122, 293);
+            this.richTextBoxDuration.Name = "richTextBoxDuration";
+            this.richTextBoxDuration.Size = new System.Drawing.Size(250, 40);
+            this.richTextBoxDuration.TabIndex = 9;
+            this.richTextBoxDuration.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(14, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Duration:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(17, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 44);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Save Tags!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(17, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(241, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 455);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VisualMp3";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxTittle;
+        private System.Windows.Forms.RichTextBox richTextBoxArtist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBoxAlbum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxDuration;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBoxName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using TagLib;
+
+namespace Visual_Mp3
+{
+    public partial class Form1 : Form
+    {
+        private string ErrorSearchFile = "The specified path is not correct or the file extension is invalid";
+        private string FileWay;
+        private File tfile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string OpenDialog()
+        {
+            var opd = new OpenFileDialog();
+            opd.Filter = "*.mp3 | *.mp3";
+            if (opd.ShowDialog() == DialogResult.OK){
+                return opd.FileName;
+            }
+            return ErrorSearchFile;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileWay = OpenDialog();
+            if (FileWay == ErrorSearchFile)
+                MessageBox.Show(ErrorSearchFile, "Fatal Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else 
+            {
+                tfile = File.Create(FileWay);
+                string title        = tfile.Tag.Title;
+                string[] artist     = tfile.Tag.Artists;
+                string album        = tfile.Tag.Album;
+                TimeSpan duration   = tfile.Properties.Duration;
+                string comment      = tfile.Tag.Comment;
+
+                richTextBoxArtist.Text      = string.Join(" ", artist);
+                richTextBoxTittle.Text      = title;
+                richTextBoxAlbum.Text       = album;
+                richTextBoxDuration.Text    = duration.ToString();
+                richTextBoxName.Text        = comment;
+            }
+        }
+
+        private void richTextBoxTittle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tfile.Tag.Title     = richTextBoxTittle.Text;
+            tfile.Tag.Artists   = richTextBoxArtist.Text.Split(' ');
+            tfile.Tag.Album     = richTextBoxAlbum.Text;
+            tfile.Tag.Comment   = richTextBoxName.Text;
+
+            tfile.Save();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBoxArtist.Clear();
+            richTextBoxTittle.Clear();
+            richTextBoxAlbum.Clear();
+            richTextBoxDuration.Clear();
+            richTextBoxName.Clear();
+
+            FileWay = "";
+        }
+    }
+}
