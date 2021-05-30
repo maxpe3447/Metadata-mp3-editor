66c2ba5ad91e47db781690c140daa35b9e42bc7c
Asya <82118916+a-sya@users.noreply.github.com>
2021-05-30 22:12:12 +0300

    +commment

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using TagLib;
+using TagLib;                       // Стороняя библиотека с открытым исходным кодом
 
 namespace Visual_Mp3
 {
@@ -8,16 +8,17 @@ namespace Visual_Mp3
     {
         private string ErrorSearchFile = "The specified path is not correct or the file extension is invalid";
         private string FileWay;
-        private File tfile;
+        private File tfile;                 // Переменная для хранения информации про файл
         public Form1()
         {
             InitializeComponent();
         }
-        private string OpenDialog()
+        private string OpenDialog()         // Ф-ция для открытия проводника
         {
             var opd = new OpenFileDialog();
-            opd.Filter = "*.mp3 | *.mp3";
-            if (opd.ShowDialog() == DialogResult.OK){
+            opd.Filter = "*.mp3 | *.mp3";   // Поиск именно mp3-файлов
+            if (opd.ShowDialog() == DialogResult.OK)
+            {
                 return opd.FileName;
             }
             return ErrorSearchFile;
@@ -27,20 +28,21 @@ namespace Visual_Mp3
             FileWay = OpenDialog();
             if (FileWay == ErrorSearchFile)
                 MessageBox.Show(ErrorSearchFile, "Fatal Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            else 
+            else
             {
                 tfile = File.Create(FileWay);
-                string title        = tfile.Tag.Title;
-                string[] artist     = tfile.Tag.Artists;
-                string album        = tfile.Tag.Album;
-                TimeSpan duration   = tfile.Properties.Duration;
-                string comment      = tfile.Tag.Comment;
+                string title = tfile.Tag.Title;              // Название
+                string[] artist = tfile.Tag.Artists;            // Артист/артисты
+                string album = tfile.Tag.Album;              // Альбом
+                TimeSpan duration = tfile.Properties.Duration;    // Длина песни
+                string comment = tfile.Tag.Comment;            // Коментарии к песне
 
-                richTextBoxArtist.Text      = string.Join(" ", artist);
-                richTextBoxTittle.Text      = title;
-                richTextBoxAlbum.Text       = album;
-                richTextBoxDuration.Text    = duration.ToString();
-                richTextBoxName.Text        = comment;
+                // Вывод даных
+                richTextBoxArtist.Text = string.Join(" ", artist);
+                richTextBoxTittle.Text = title;
+                richTextBoxAlbum.Text = album;
+                richTextBoxDuration.Text = duration.ToString();
+                richTextBoxName.Text = comment;
             }
         }
 
@@ -51,16 +53,18 @@ namespace Visual_Mp3
 
         private void button2_Click(object sender, EventArgs e)
         {
-            tfile.Tag.Title     = richTextBoxTittle.Text;
-            tfile.Tag.Artists   = richTextBoxArtist.Text.Split(' ');
-            tfile.Tag.Album     = richTextBoxAlbum.Text;
-            tfile.Tag.Comment   = richTextBoxName.Text;
+            // Сохранение изменений в тегах
+            tfile.Tag.Title = richTextBoxTittle.Text;
+            tfile.Tag.Artists = richTextBoxArtist.Text.Split(' ');
+            tfile.Tag.Album = richTextBoxAlbum.Text;
+            tfile.Tag.Comment = richTextBoxName.Text;
 
             tfile.Save();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            // Очистка полей с тегами
             richTextBoxArtist.Clear();
             richTextBoxTittle.Clear();
             richTextBoxAlbum.Clear();
@@ -68,6 +72,11 @@ namespace Visual_Mp3
             richTextBoxName.Clear();
 
             FileWay = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
